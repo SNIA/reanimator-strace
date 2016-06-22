d2fc4b91ad4899708142cfee8b6d79a6eb1697ec
Nina HMC Brown <nbrown@trex.fsl.cs.sunysb.edu>
2016-06-22 12:25:18 -0400

    Added system call Mkdir

--- syscall.c
+++ syscall.c
@@ -1199,6 +1199,11 @@ trace_syscall_exiting(struct tcb *tcp)
 			ds_write_record(ds_module, "chdir", tcp->u_arg,
 					common_fields, v_args);
 			break;
+		case SEN_mkdir: /* Mkdir system call */
+			v_args[0] = ds_get_path(tcp, tcp->u_arg[0]);
+			ds_write_record(ds_module, "mkdir", tcp->u_arg,
+					common_fields, v_args);
+			break;
 	}
 	/* Free memory allocated to v_args. */
 	for (i = 0; i < DS_MAX_ARGS; i++) {
