40aaf65f0983a15899559d95caf21f8bcb6201dc
Nina HMC Brown <nbrown@trex.fsl.cs.sunysb.edu>
2016-06-27 16:31:38 -0400

    Fixed a typo that occurred when merging.

--- syscall.c
+++ syscall.c
@@ -1244,6 +1244,8 @@ trace_syscall_exiting(struct tcb *tcp)
 		case SEN_chmod: /* Chmod system call */
 			v_args[0] = ds_get_path(tcp, tcp->u_arg[0]);
 			ds_write_record(ds_module, "chmod", tcp->u_arg,
+					common_fields, v_args);
+			break;
 		case SEN_lseek: /* LSeek system call */
 			ds_write_record(ds_module, "lseek", tcp->u_arg,
 					common_fields, NULL);
