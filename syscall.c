2b456b1562dfaed9591cb2ff256389a7a41ea6b6
Nina HMC Brown <nbrown@trex.fsl.cs.sunysb.edu>
2016-06-28 12:42:09 -0400

    Added system call Chmod

--- syscall.c
+++ syscall.c
@@ -1262,6 +1262,11 @@ trace_syscall_exiting(struct tcb *tcp)
 			ds_write_record(ds_module, "pwrite", tcp->u_arg,
 					common_fields, v_args);
 			break;
+		case SEN_chown: /* Chown system call */
+			v_args[0] = ds_get_path(tcp, tcp->u_arg[0]);
+			ds_write_record(ds_module, "chown", tcp->u_arg,
+					common_fields, v_args);
+			break;
 	}
 	/* Free memory allocated to v_args. */
 	for (i = 0; i < DS_MAX_ARGS; i++) {
