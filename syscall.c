77f314704a0da08c947187885110bb23ce5bea07
Shubhi Rani <shubhi@trex.fsl.cs.sunysb.edu>
2016-07-29 14:59:13 -0400

    Added mkdirat system call in fsl-strace

--- syscall.c
+++ syscall.c
@@ -1308,6 +1308,10 @@ trace_syscall_exiting(struct tcb *tcp)
 			ds_write_record(ds_module, "mkdir", tcp->u_arg,
 					common_fields, v_args);
 			break;
+		case SEN_mkdirat: /* Mkdirat system call */
+			v_args[0] = ds_get_path(tcp, tcp->u_arg[1]);
+			ds_write_record(ds_module, "mkdirat", tcp->u_arg,
+					common_fields, v_args);
 		case SEN_rmdir: /* Rmdir system call */
 			v_args[0] = ds_get_path(tcp, tcp->u_arg[0]);
 			ds_write_record(ds_module, "rmdir", tcp->u_arg,
