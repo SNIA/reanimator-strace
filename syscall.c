1f61c5aa3229857a8b59a8312ddbd4d9bff78c1f
Leixiang Wu <leixiang.wu@stonybrook.edu>
2017-02-28 15:26:35 -0500

    Add break in getrlimit/setrlimit switch statement

--- syscall.c
+++ syscall.c
@@ -1435,16 +1435,19 @@ trace_syscall_exiting(struct tcb *tcp)
 		case SEN_vfork: /* VFork system call */
 			ds_write_record(ds_module, "vfork", tcp->u_arg,
 					common_fields, v_args);
+			break;
 		case SEN_setrlimit: /* Setrlimit system call */
 			v_args[0] = ds_get_buffer(tcp, tcp->u_arg[1],
 				sizeof(struct rlimit));
 			ds_write_record(ds_module, "setrlimit", tcp->u_arg,
 				common_fields, v_args);
+			break;
 		case SEN_getrlimit: /* Getrlimit system call */
 			v_args[0] = ds_get_buffer(tcp, tcp->u_arg[1],
 				sizeof(struct rlimit));
 			ds_write_record(ds_module, "getrlimit", tcp->u_arg,
 				common_fields, v_args);
+			break;
 		/*
 		 * These system calls are chosen not be traced by
 		 * fsl-strace.
